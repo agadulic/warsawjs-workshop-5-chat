e4848e5bd5009f24ae26d0b999886be7de5eb3aa
AgaDulic <mrs.agnieszka.michalak@gmail.com>
2017-04-23 17:29:10 +0200

    knex, json, migrations, data base

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    production: {
+        client: 'sqlite3',
+        useNullAsDefault: true,
+        connection: {
+            filename: './db/chat_users.db'
+        },
+        migrations: {
+            directory: './db/migrations'
+        }
+    }
+
+};
